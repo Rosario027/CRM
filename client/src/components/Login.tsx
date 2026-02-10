e219f155158a9777542cdca0f71f15de92286eb0
akash <jrakash027@gmail.com>
2026-02-10 22:21:31 +0530

    Fix: Remove hardcoded localhost URL

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -18,7 +18,7 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
         setLoading(true);
 
         try {
-            const response = await fetch('http://localhost:5000/api/login', {
+            const response = await fetch('/api/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
