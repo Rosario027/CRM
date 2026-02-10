06eb14a579c9dccb9a4e5c33124e197468d54fbc
akash <jrakash027@gmail.com>
2026-02-10 22:48:48 +0530

    Fix: Remove unused imports and dead links

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,13 +1,5 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import {
-    rxDashboard,
-    rxPerson,
-    rxCalendar,
-    rxClipboardList,
-    rxFileText,
-    rxGear
-} from 'react-icons/rx'; // Using Radix Icons for typical dashboard look
 // Or Lucide (already installed)
 import {
     LayoutDashboard,
